cec52764b600f435eee21d55dea496a3f62b1a92
Aydin Terzi <aydinterzi7@gmail.com>
2024-12-23 21:01:17 +0300

    29 big waves

---
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -0,0 +1,16 @@
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uTime;
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime) * sin(modelPosition.z * uBigWavesFrequency.y + uTime) * uBigWavesElevation;
+
+    modelPosition.y += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition; 
+}
